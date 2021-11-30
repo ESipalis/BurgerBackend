dac184a5f8e075905916e1cd159938ad43888095
ESipalis <eimantas98@gmail.com>
2021-11-30 02:18:46 +0100

    Domain: Updated `Review` to allow nullable `Comment` and `ImageId`.

--- Domain/Review/Review.cs
+++ Domain/Review/Review.cs
@@ -2,4 +2,4 @@
 
 namespace BurgerBackend.Domain.Review;
 
-public record Review(long Id, DateTimeOffset CreatedAt, Rating Rating, string Comment, Guid ImageId, User.User User);
+public record Review(long Id, DateTimeOffset CreatedAt, Rating Rating, string? Comment, Guid? ImageId, User.User User);
